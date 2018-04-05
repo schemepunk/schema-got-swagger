d23e1dfb2e1ca267e14dc1cd7f92316ba02883f5
Patrick Coffey <patrickcoffey48@gmail.com>
2018-04-05 16:43:19 -0500

    test(semverish): snapshot testing for dir -> semverish obj

--- __tests__/lib/directoryToSemverishObject.js
+++ __tests__/lib/directoryToSemverishObject.js
@@ -8,10 +8,11 @@ const directoryToSemverishObject = require('../../lib/directoryToSemverishObject
 const semverishObject = require('../__helpers__/semverishObject.json');
 
 describe('directoryToSemverishObject', () => {
-  it ('Can return a semverish object from a given path to a semverish directory', () => {
-    directoryToSemverishObject(path.join(__dirname, `../__helpers__/semverishObject`))
+  test('Can return a semverish object from a given path to a semverish directory', () => {
+    expect.assertions(1);
+    return directoryToSemverishObject(path.join(__dirname, `../__helpers__/semverishObject`))
       .then((outputObj) => {
-        expect(outputObj).toEqual(semverishObject);
+        expect(outputObj).toMatchSnapshot();
       });
   });
 });
