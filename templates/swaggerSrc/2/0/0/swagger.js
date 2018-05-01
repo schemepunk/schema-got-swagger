7270c88db99efa242567cad8506d00e22ee39968
Patrick Coffey <patrickcoffey48@gmail.com>
2018-05-01 16:20:16 -0500

    fix(template): do not escape special chars in termsOfService url

--- templates/swaggerSrc/2/0/0/swagger.js
+++ templates/swaggerSrc/2/0/0/swagger.js
@@ -3,7 +3,7 @@ module.exports = {
   title: '{{apiName}}',
   description: '{{apiDescription}}',
   version: '{{#apiVersion}}{{apiVersion}}{{/apiVersion}}{{^apiVersion}}{{apiSemver}}{{/apiVersion}}',
-  termsOfService: '{{termsOfService}}',
+  termsOfService: '{{&termsOfService}}',
   contact: '{{=<% %>=}}{<%={{ }}=%> "name": "{{contact.name}}", "url": "{{&contact.url}}", "email": "{{contact.email}}" {{=<% %>=}}}<%={{ }}=%>', // eslint-disable-line max-len
   license: '{{=<% %>=}}{ "name": "<%license.name%>", "url": "<%&license.url%>"}<%={{ }}=%>',
   host: '{{apiHost}}',
